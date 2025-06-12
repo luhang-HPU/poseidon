eae4f141869ae08ccb7803fa6703a1d1465f317f
sunshuo <110672056+Darcy-Sun@users.noreply.github.com>
2025-06-12 14:33:23 +0800

    rename: move hardware header to hardware project (#10)

--- src/poseidon_context.h
+++ src/poseidon_context.h
@@ -2,9 +2,12 @@
 
 #include "basics/randomgen.h"
 #include "crt_context.h"
-#include "hardware_include/hardware_context.h"
 #include "parameters_literal.h"
 #include "util/pke_params_defines.h"
+
+#ifdef USING_HARDWARE
+#include "poseidon_hardware/hardware_context.h"
+#endif
 
 using namespace std;
 
@@ -30,10 +33,12 @@ public:
 
     POSEIDON_NODISCARD inline shared_ptr<CrtContext> crt_context() const { return crt_context_; }
 
+#ifdef USING_HARDWARE
     POSEIDON_NODISCARD inline shared_ptr<HardwareContext> hardware_context() const
     {
         return hardware_context_;
     }
+#endif
 
     POSEIDON_NODISCARD inline bool using_hardware() const { return using_hardware_; }
 
@@ -52,12 +57,14 @@ private:
     std::shared_ptr<const poseidon::ParametersLiteral> parameters_literal_{nullptr};
     std::shared_ptr<CrtContext> crt_context_{nullptr};
 
+#ifdef USING_HARDWARE
     std::shared_ptr<HardwareContext> hardware_context_{nullptr};
+#endif
 
-    bool using_hardware_ = false;
     std::shared_ptr<UniformRandomGeneratorFactory> random_generator_{nullptr};
     sec_level_type sec_level_ = sec_level_type::none;
     KeySwitchVariant key_switch_variant_ = GHS;
+    bool using_hardware_ = false;
 };
 
 }  // namespace poseidon
