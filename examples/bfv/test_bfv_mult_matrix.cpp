1a35d5a388ffbbd116193072139995c57c960302
tangjiajian <1308768831@qq.com>
2025-07-21 15:42:22 +0800

    fix: rename

--- examples/bfv/test_bfv_mult_matrix.cpp
+++ examples/bfv/test_bfv_mult_matrix.cpp
@@ -19,21 +19,21 @@ int main()
         PoseidonFactory::get_instance()->create_poseidon_context(bfv_param_literal);
     auto bfv_eva = PoseidonFactory::get_instance()->create_bfv_evaluator(context);
 
-    BatchEncoder enc(context);
+    BatchEncoder encoder(context);
     KeyGenerator keygen(context);
     PublicKey public_key;
     GaloisKeys galois_keys;
     keygen.create_public_key(public_key);
-    Encryptor encryptor(context, public_key);
-    Decryptor decryptor(context, keygen.secret_key());
+    Encryptor enc(context, public_key);
+    Decryptor dec(context, keygen.secret_key());
 
     // init Plaintext and Ciphertext
-    Plaintext plain, plain_res;
-    Ciphertext cipher, cipher_res;
+    Plaintext plt, plt_res;
+    Ciphertext ct, ct_res;
     int mat_size = bfv_param_literal.slot();
     auto level = bfv_param_literal.q().size() - 1;
-    vector<uint64_t> message(mat_size, 1);
-    sample_random_vector(message, mat_size, 10);
+    vector<uint64_t> msg(mat_size, 1);
+    sample_random_vector(msg, mat_size, 10);
     vector<uint64_t> vec_result(mat_size, 0);
     std::vector<vector<uint64_t>> mat_t1(mat_size);
     std::vector<vector<uint64_t>> mat(mat_size, vector<uint64_t>(mat_size, 0));
@@ -43,12 +43,12 @@ int main()
     }
 
     // GenMatrices
-    vector<uint64_t> message_tmp;
+    vector<uint64_t> msg_tmp;
     MatrixPlain matrix_plain;
     auto coeff_mod = bfv_param_literal.plain_modulus();
-    matrix_operations::matrix_vector_multiply_mod(mat, message, message_tmp, coeff_mod.value());
+    matrix_operations::matrix_vector_multiply_mod(mat, msg, msg_tmp, coeff_mod.value());
     matrix_operations::rotate_slots_matrix(mat, mat_t1);
-    gen_matrix_form_bsgs(matrix_plain, matrix_plain.rot_index, enc, mat_t1, level, 1,
+    gen_matrix_form_bsgs(matrix_plain, matrix_plain.rot_index, encoder, mat_t1, level, 1,
                          bfv_param_literal.log_slots());
 
     // init keys
@@ -64,23 +64,23 @@ int main()
     keygen.create_galois_keys(rot_index_tmp, galois_keys);
 
     // encode
-    enc.encode(message, plain);
-    encryptor.encrypt(plain, cipher);
+    encoder.encode(msg, plt);
+    enc.encrypt(plt, ct);
 
     auto start = chrono::high_resolution_clock::now();
     Timestacs timestacs;
     timestacs.start();
-    bfv_eva->multiply_by_diag_matrix_bsgs(cipher, matrix_plain, cipher_res, galois_keys);
+    bfv_eva->multiply_by_diag_matrix_bsgs(ct, matrix_plain, ct_res, galois_keys);
     timestacs.end();
     timestacs.print_time("MULT MATRIX : ");
-    bfv_eva->read(cipher_res);
+    bfv_eva->read(ct_res);
 
     // decode & decrypt
-    decryptor.decrypt(cipher_res, plain_res);
-    enc.decode(plain_res, vec_result);
+    dec.decrypt(ct_res, plt_res);
+    encoder.decode(plt_res, vec_result);
     for (int i = 0; i < 4; i++)
     {
-        printf("soft vec[%d]   : %ld  \n", i, message_tmp[i]);
+        printf("soft vec[%d]   : %ld  \n", i, msg_tmp[i]);
         printf("result vec[%d] : %ld  \n", i, vec_result[i]);
     }
 }
--- examples/bgv/test_bgv_load.cpp
+++ examples/bgv/test_bgv_load.cpp
@@ -1,7 +1,3 @@
-//
-// Created by Lenovo on 2025/3/17.
-//
-
 #include "src/decryptor.h"
 #include "src/encryptor.h"
 #include "src/factory/poseidon_factory.h"
@@ -13,17 +9,21 @@ using namespace poseidon::util;
 
 int main()
 {
+    cout << BANNER << std::endl;
+    cout << "POSEIDON SOFTWARE VERSION:" << POSEIDON_VERSION << std::endl;
+    cout << "" << std::endl;
+
     ParametersLiteralDefault bgv_param_literal(BGV, 4096, sec_level_type::tc128);
     PoseidonFactory::get_instance()->set_device_type(DEVICE_SOFTWARE);
     auto context = PoseidonFactory::get_instance()->create_poseidon_context(bgv_param_literal);
     auto bgv_eva = PoseidonFactory::get_instance()->create_bgv_evaluator(context);
     BatchEncoder encoder(context);
     KeyGenerator keygen(context);
-    PublicKey publicKey;
+    PublicKey pub_key;
 
-    keygen.create_public_key(publicKey);
+    keygen.create_public_key(pub_key);
 
-    Encryptor encryptor(context, publicKey);
+    Encryptor encryptor(context, pub_key);
     Decryptor decryptor(context, keygen.secret_key());
 
     vector<uint64_t> vec_data = {97, 98, 99};
@@ -33,25 +33,18 @@ int main()
     encoder.encode(vec_data, plain);
     encryptor.encrypt(plain, ciph);
 
-    ciph_load.unsafe_load(context,
-                          reinterpret_cast<const poseidon_byte *>(ciph.data()),
-                          ciph.size());
+    stringstream ss;
+    ciph.save(ss);
+    ciph_load.unsafe_load(context, ss);
 
     decryptor.decrypt(ciph_load, plain_load);
     vector<uint64_t> vec_res;
     encoder.decode(plain_load, vec_res);
 
-    if (vec_data.size() != vec_res.size())
-    {
-        return -1;
-    }
     for (auto i = 0; i < vec_data.size(); ++i)
     {
-        if (vec_data[i] != vec_res[i])
-        {
-            std::cout << "error" << std::endl;
-            return -1;
-        }
+        printf("source_data[%d]   : %ld  \n", i, vec_data[i]);
+        printf("result_data[%d]   : %ld  \n", i, vec_res[i]);
     }
 
     return 0;
