d77e9c39754685bbd8359fee87aec17e26dff621
tangjiajian <1308768831@qq.com>
2025-11-25 17:23:58 +0800

    fix: bug

--- src/poseidon/houmo/houmo_api.cpp
+++ src/poseidon/houmo/houmo_api.cpp
@@ -17,6 +17,11 @@ void HOUMO_API::houmo_add(const int16_t* op1, const int16_t* op2, int16_t* res, 
     // Get the total number of inputs
     int input_num = module_add_.GetInputNum();
 
+    // create buffer on host, and transfer data
+    tcim::Buffer input_x_buf = tcim::Buffer::CreateHostBuffer(2 * size, &op1);
+    tcim::Buffer input_y_buf = tcim::Buffer::CreateHostBuffer(2 * size, &op2);
+    tcim::Buffer output_buf = tcim::Buffer::CreateHostBuffer(2 * size);
+
     // For each input:
     for (int idx = 0; idx < input_num; idx++)
     {
@@ -26,20 +31,22 @@ void HOUMO_API::houmo_add(const int16_t* op1, const int16_t* op2, int16_t* res, 
         auto input_info = module_add_.GetInputInfo(input_name).AsContiguous();
         // Allocate memory on host CPU for storing input data
         auto input_tensor = tcim::Tensor::CreateHostTensor(input_info, 2 * size);
+
+        // copy buffer to tensor buffer
+        if (idx == 0)
+        {
+            stat = input_x_buf.CopyTo(input_tensor.Buffer());
+            std::cout << "copy1 status: " << stat << std::endl;
+        }
+        else if (idx == 1)
+        {
+            stat = input_y_buf.CopyTo(input_tensor.Buffer());
+            std::cout << "copy2 status: " << stat << std::endl;
+        }
+
         // Create a map between input name and input tensor
         input_map.insert(std::pair<std::string, tcim::Tensor>(input_name, input_tensor));
     }
-
-    // create buffer on host, and transfer data
-    tcim::Buffer input_x_buf = tcim::Buffer::CreateHostBuffer(2 * size, &op1);
-    tcim::Buffer input_y_buf = tcim::Buffer::CreateHostBuffer(2 * size, &op2);
-    tcim::Buffer output_buf = tcim::Buffer::CreateHostBuffer(2 * size);
-
-    // copy buffer to tensor buffer
-    stat = input_x_buf.CopyTo(input_op1_tensor_.Buffer());
-    std::cout << "copy1 status: " << stat << std::endl;
-    stat = input_y_buf.CopyTo(input_op2_tensor_.Buffer());
-    std::cout << "copy2 status: " << stat << std::endl;
 
     // Loop through each key-value pair in the input_map
     for (const auto& input : input_map)
@@ -74,11 +81,7 @@ void HOUMO_API::houmo_add(const int16_t* op1, const int16_t* op2, int16_t* res, 
     {
         // Get each output with the key-value pair from the output_map
         module_add_.GetOutput(output.first, output.second);
-    }
 
-
-    for (auto &output: output_map)
-    {
         output.second.Buffer().CopyTo(output_buf, 2 * size);
         output_buf.CopyToHost(res, 2 * size);
     }
@@ -92,6 +95,11 @@ void HOUMO_API::houmo_sub(const int16_t* op1, const int16_t* op2, int16_t* res, 
     // Get the total number of inputs
     int input_num = module_sub_.GetInputNum();
 
+    // create buffer on host, and transfer data
+    tcim::Buffer input_x_buf = tcim::Buffer::CreateHostBuffer(2 * size, &op1);
+    tcim::Buffer input_y_buf = tcim::Buffer::CreateHostBuffer(2 * size, &op2);
+    tcim::Buffer output_buf = tcim::Buffer::CreateHostBuffer(2 * size);
+
     // For each input:
     for (int idx = 0; idx < input_num; idx++)
     {
@@ -101,20 +109,22 @@ void HOUMO_API::houmo_sub(const int16_t* op1, const int16_t* op2, int16_t* res, 
         auto input_info = module_sub_.GetInputInfo(input_name).AsContiguous();
         // Allocate memory on host CPU for storing input data
         auto input_tensor = tcim::Tensor::CreateHostTensor(input_info, 2 * size);
+
+        // copy buffer to tensor buffer
+        if (idx == 0)
+        {
+            stat = input_x_buf.CopyTo(input_tensor.Buffer());
+            std::cout << "copy1 status: " << stat << std::endl;
+        }
+        else if (idx == 1)
+        {
+            stat = input_y_buf.CopyTo(input_tensor.Buffer());
+            std::cout << "copy2 status: " << stat << std::endl;
+        }
+
         // Create a map between input name and input tensor
         input_map.insert(std::pair<std::string, tcim::Tensor>(input_name, input_tensor));
     }
-
-    // create buffer on host, and transfer data
-    tcim::Buffer input_x_buf = tcim::Buffer::CreateHostBuffer(2 * size, &op1);
-    tcim::Buffer input_y_buf = tcim::Buffer::CreateHostBuffer(2 * size, &op2);
-    tcim::Buffer output_buf = tcim::Buffer::CreateHostBuffer(2 * size);
-
-    // copy buffer to tensor buffer
-    stat = input_x_buf.CopyTo(input_op1_tensor_.Buffer());
-    std::cout << "copy1 status: " << stat << std::endl;
-    stat = input_y_buf.CopyTo(input_op2_tensor_.Buffer());
-    std::cout << "copy2 status: " << stat << std::endl;
 
     // Loop through each key-value pair in the input_map
     for (const auto& input : input_map)
@@ -149,11 +159,7 @@ void HOUMO_API::houmo_sub(const int16_t* op1, const int16_t* op2, int16_t* res, 
     {
         // Get each output with the key-value pair from the output_map
         module_sub_.GetOutput(output.first, output.second);
-    }
 
-
-    for (auto &output: output_map)
-    {
         output.second.Buffer().CopyTo(output_buf, 2 * size);
         output_buf.CopyToHost(res, 2 * size);
     }
@@ -167,6 +173,12 @@ void HOUMO_API::houmo_mul(const int16_t* op1, const int16_t* op2, int16_t* res, 
     // Get the total number of inputs
     int input_num = module_mul_.GetInputNum();
 
+    // create buffer on host, and transfer data
+    tcim::Buffer input_x_buf = tcim::Buffer::CreateHostBuffer(2 * size, &op1);
+    tcim::Buffer input_y_buf = tcim::Buffer::CreateHostBuffer(2 * size, &op2);
+    tcim::Buffer output_buf = tcim::Buffer::CreateHostBuffer(2 * size);
+
+
     // For each input:
     for (int idx = 0; idx < input_num; idx++)
     {
@@ -176,20 +188,22 @@ void HOUMO_API::houmo_mul(const int16_t* op1, const int16_t* op2, int16_t* res, 
         auto input_info = module_mul_.GetInputInfo(input_name).AsContiguous();
         // Allocate memory on host CPU for storing input data
         auto input_tensor = tcim::Tensor::CreateHostTensor(input_info, 2 * size);
+
+        // copy buffer to tensor buffer
+        if (idx == 0)
+        {
+            stat = input_x_buf.CopyTo(input_tensor.Buffer());
+            std::cout << "copy1 status: " << stat << std::endl;
+        }
+        else if (idx == 1)
+        {
+            stat = input_y_buf.CopyTo(input_tensor.Buffer());
+            std::cout << "copy2 status: " << stat << std::endl;
+        }
+
         // Create a map between input name and input tensor
         input_map.insert(std::pair<std::string, tcim::Tensor>(input_name, input_tensor));
     }
-
-    // create buffer on host, and transfer data
-    tcim::Buffer input_x_buf = tcim::Buffer::CreateHostBuffer(2 * size, &op1);
-    tcim::Buffer input_y_buf = tcim::Buffer::CreateHostBuffer(2 * size, &op2);
-    tcim::Buffer output_buf = tcim::Buffer::CreateHostBuffer(2 * size);
-
-    // copy buffer to tensor buffer
-    stat = input_x_buf.CopyTo(input_op1_tensor_.Buffer());
-    std::cout << "copy1 status: " << stat << std::endl;
-    stat = input_y_buf.CopyTo(input_op2_tensor_.Buffer());
-    std::cout << "copy2 status: " << stat << std::endl;
 
     // Loop through each key-value pair in the input_map
     for (const auto& input : input_map)
@@ -224,11 +238,7 @@ void HOUMO_API::houmo_mul(const int16_t* op1, const int16_t* op2, int16_t* res, 
     {
         // Get each output with the key-value pair from the output_map
         module_mul_.GetOutput(output.first, output.second);
-    }
 
-
-    for (auto &output: output_map)
-    {
         output.second.Buffer().CopyTo(output_buf, 2 * size);
         output_buf.CopyToHost(res, 2 * size);
     }
