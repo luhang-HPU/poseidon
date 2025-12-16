2193e3527937249ece985643dda34c4b0bd30a09
sunshuo <ss_0926@126.com>
2025-12-16 16:34:17 +0800

    fix

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+PACKAGE_NAME="torch"
+PACKAGE_PATH=$(pip show "$PACKAGE_NAME" 2>/dev/null | grep -i "^Location:" | awk '{print $2}')
+export LD_LIBRARY_PATH="${PACKAGE_PATH}/${PACKAGE_NAME}/lib":$LD_LIBRARY_PATH
+
+# 在运行可执行文件之前 需要先将上述命令在对应窗口执行
